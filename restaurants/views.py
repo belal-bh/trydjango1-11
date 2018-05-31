6226e0d1a32ea498510f7a18a46342c6059c842a
belal-bh <bh.pro.pust@gmail.com>
2018-06-01 01:08:16 +0600

    Updated views, function based view, HttpRespons

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# Function based view
+
+
+def home(request):
+    return HttpResponse("Hello")
+    # return render(request, "home.html", {})  # response
