76b50eb13fbe574ff0d3df3a63d04593de5862bc
belal-bh <bh.pro.pust@gmail.com>
2018-06-06 19:41:44 +0600

    Use TemplateView class

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,32 +2,17 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-
-# Create your views here.
-# Function based view
-
-
-def home(request):
-    context = {
-    }
-    return render(request, "home.html", context)
-
-
-def about(request):
-    context = {
-    }
-    return render(request, "about.html", context)
-
-
-def contact(request):
-    context = {
-    }
-    return render(request, "contact.html", context)
+from django.views.generic import TemplateView
 
 
 # Class based view
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        print(kwargs)
-        context = {}
-        return render(request, "contact.html", context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = random.randint(0, 10000000000)
+        context = {
+            'num': num
+        }
+        return context
--- sample_project/urls.py
+++ sample_project/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from restaurants.views import home, about, contact, ContactView
+from restaurants.views import HomeView
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-    url(r'^about/$', about),
-    url(r'^contact/(?P<id>\d+)/$', ContactView.as_view()),
+    url(r'^$', HomeView.as_view()),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
 ]
